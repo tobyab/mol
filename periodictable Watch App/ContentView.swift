015841b596f578542d5a8232051d87fbddeb3183
Toby Brown <hi@tobyb.dev>
2024-02-24 17:07:25 +0000

    Initial Commit

---
+++ periodictable Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  periodictable Watch App
+//
+//  Created by Toby Brown on 24/02/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ periodictable Watch App/periodictableApp.swift
@@ -0,0 +1,17 @@
+//
+//  periodictableApp.swift
+//  periodictable Watch App
+//
+//  Created by Toby Brown on 24/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct periodictable_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
